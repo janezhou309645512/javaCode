12cd8c380f8fce1ff33dde71f3cd84c6153f6787
janezhou309645512 <309645512@qq.com>
2018-05-23 17:13:09 +0800

    第一次
    第一次

---
+++ AppFristAreaDTO.java
@@ -0,0 +1,25 @@
+package com.lingsheng.messervice.dto;
+/**
+ * 区域
+ * @author Mungrry.liu
+ *
+ */
+public class AppFristAreaDTO {
+	private String fname;
+	private int fid;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public int getFid() {
+		return fid;
+	}
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+	
+	
+
+}
